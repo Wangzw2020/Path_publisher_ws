e7e5c492cc566aa114bb1f988adb0a4946993577
Wangzw2020 <895700962@qq.com>
2022-05-20 15:49:07 +0800

    2

--- src/path_publisher/include/path_publisher.h
+++ src/path_publisher/include/path_publisher.h
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include "std_msgs/Int8.h"
 #include <path_msgs/path_msg.h>
+#include <fstream>
+#include <vector>
 
 class PathPublisher
 {
@@ -13,14 +15,22 @@ public:
 	
 	void path_state_callback(const std_msgs::Int8::ConstPtr& path_state);
 	
+	bool loadAllPathFile();
+	bool pubNextPath();
+	
 private:
     ros::NodeHandle nh_, nh_private_;
     bool is_ready_;
     
-    std::string state_topic_, path_name_topic_;
+    std::string task_list_file_;
+    std::vector<std::string> all_path_;
+    
+    std::string state_topic_, path_topic_;
     ros::Publisher path_file_name_pub_;
 	
 	ros::Subscriber path_state_sub_;
     std_msgs::Int8 path_tracking_state_;
     
+    int current_task_;
+    int num_task_;
 };
--- src/path_publisher/src/path_publisher.cpp
+++ src/path_publisher/src/path_publisher.cpp
@@ -12,9 +12,18 @@ bool PathPublisher::init(ros::NodeHandle nh, ros::NodeHandle nh_private)
 	nh_private_ = nh_private;
 	
 	nh_private_.param<std::string>("state_topic", state_topic_, " ");
-	nh_private_.param<std::string>("path_name_topic", path_name_topic_, " ");
+	nh_private_.param<std::string>("path_topic", path_topic_, " ");
+	nh_private_.param<std::string>("task_list_file", task_list_file_, " ");
+	nh_private_.param<int>("num_task", num_task_, 0);
+	std::cout << "num of task is " << num_task_ << std::endl;
+	num_task_ = num_task_-1;
+	current_task_ = 0;
 	
-	path_file_name_pub_ = nh_.advertise<path_msgs::path_msg>(path_name_topic_, 1);
+	if (!loadAllPathFile())
+	{
+		ROS_ERROR("[%s] Task file load failed!", __NAME__);
+	}
+	path_file_name_pub_ = nh_.advertise<path_msgs::path_msg>(path_topic_, 1);
 	
 	path_state_sub_ = nh_.subscribe(state_topic_, 1, &PathPublisher::path_state_callback, this);
 	
@@ -35,16 +44,17 @@ void PathPublisher::path_state_callback(const std_msgs::Int8::ConstPtr& path_sta
 	
 	if(state == 0)		//done
 	{
-		std::cout << "state = 0  Task is done!" << std::endl;
+		std::cout << "state = 0  Task" << current_task_ << "is done!" << std::endl;
 		
-		path_msgs::path_msg next_path;
-		next_path.task = next_path.DRIVE_TASK;
-		next_path.type = next_path.FILE_TYPE;
-		next_path.path_resolution = 0.1;
-		next_path.expect_speed = 15;
-		next_path.roadnet_file = "a.txt";
-		next_path.path_filp = false;
-		path_file_name_pub_.publish(next_path);
+		if (current_task_ == num_task_)
+		{
+			ROS_INFO("[%s] All task is done! ", __NAME__);
+		}
+		
+		if (!pubNextPath())
+		{
+			ROS_ERROR("[%s] compute next path failed!", __NAME__);
+		}
 		
 		std::cout << "New task is sent!" << std::endl;
 	}
@@ -56,6 +66,84 @@ void PathPublisher::path_state_callback(const std_msgs::Int8::ConstPtr& path_sta
 	{
 		std::cout << "state = 2  Moving!" << std::endl;
 	}
+}
+
+bool PathPublisher::loadAllPathFile()
+{
+	std::ifstream task_file;
+	task_file.open(task_list_file_.c_str());
+	std::string line;
+	if (!task_file)
+	{
+		ROS_ERROR("[%s] open task file failed!", __NAME__);
+		return false;
+	}
+	while(getline(task_file,line))
+	{
+		all_path_.push_back(line);
+	}
+	task_file.close();
+
+	std::cout << all_path_.size() << " path loaded!" << std::endl;
+	
+	return true;
+}
+
+bool PathPublisher::pubNextPath()
+{
+	if (current_task_ == 0)
+	{
+		path_msgs::path_msg next_path;
+	
+		next_path.task = next_path.DRIVE_TASK;
+		next_path.type = next_path.FILE_TYPE;
+		next_path.path_resolution = 0.1;
+		next_path.expect_speed = 15;
+		next_path.roadnet_file = all_path_[0];
+		next_path.path_filp = false;
+	
+		path_file_name_pub_.publish(next_path);
+	}
+	else if (current_task_ == 1)
+	{
+		path_msgs::path_msg next_path;
+	
+		next_path.task = next_path.DRIVE_TASK;
+		next_path.type = next_path.FILE_TYPE;
+		next_path.path_resolution = 0.1;
+		next_path.expect_speed = 15;
+		next_path.roadnet_file = all_path_[1];
+		next_path.path_filp = false;
+	
+		path_file_name_pub_.publish(next_path);
+	}
+	else if (current_task_ == 2)
+	{
+	
+	}
+	else if (current_task_ == 3)
+	{
+	
+	}
+	else if (current_task_ == 4)
+	{
+	
+	}
+	
+//	path_msgs::path_msg next_path;
+//	
+//	next_path.task = next_path.DRIVE_TASK;
+//	next_path.type = next_path.FILE_TYPE;
+//	next_path.path_resolution = 0.1;
+//	next_path.expect_speed = 15;
+//	next_path.roadnet_file = "a.txt";
+//	next_path.path_filp = false;
+//	
+//	path_file_name_pub_.publish(next_path);
+	
+	++current_task_;
+	
+	return true;
 }
 
 int main(int argc, char** argv)
